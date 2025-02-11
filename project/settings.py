471934d99c853a067c4163bcbe67c3871c988111
jotap1101 <jotap1101.notebook@gmail.com>
2025-02-11 20:31:14 -0300

    update settings.py to set a hardcoded SECRET_KEY for development purposes

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+
+SECRET_KEY = '!5#s9%ui7&ibwbbsc)v$x3d@913d2o@42660me4u4vz+yyynz&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
